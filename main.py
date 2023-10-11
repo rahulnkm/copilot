52b1064d10e65eecbb2d50bc4ab358a303e99a5a
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2023-10-11 14:33:28 -0400

    test query proposals

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+import requests
 
 st.header("Lido Copilot")
 
@@ -8,8 +9,80 @@ Everything you need to know about Lido Finance.
 We've passed all prior proposals with all relevant voting information.
 Use it to get proposal summaries.
 Enter your personalized information to get suggestions.
+Talk to Lido proposals.
 """
 
-openai.api_key = st.text_input("OpenAI API Key")
-question = st.text_input("Enter your question")
+# Collect All Lido Proposals in SQL
+# Query GraphQL For Lido Info
+# Put 
+# Embed SQL into 
+# Embed All Lido Proposals
+# Pass the embedding into the chat completion and 
 
+# User Enters Question in Text Input,
+# User Presses Button
+# Button Triggers API Call
+# API Call Returns Text Answer
+# Answer Is Displayed With st.write
+# Embed all proposals
+# Chat with embedding
+
+def query_proposals():
+    url = "https://hub.snapshot.org/graphql"
+    query = """
+    query {
+        proposals (
+            first: 20,
+            skip: 0,
+            where: {
+                space_in: ["yam.eth"],
+                state: "closed"
+                },
+                orderBy: "created",
+                orderDirection: desc
+                ) {
+                    id
+                    title
+                    body
+                    choices
+                    start
+                    end    
+                    snapshot
+                    state
+                    scores
+                    scores_by_strategy
+                    scores_total
+                    scores_updated
+                    author
+                    space {
+                        id
+                        name
+                        }
+                        }
+                        }
+                        """
+    data = {'query': query}
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        st.write(response.json())
+    else:
+        st.write('Request failed with status code', response.status_code)
+
+def embed_docm(docm):
+    # embed each proposal as its own document
+    # create a 
+    return embeds
+
+def similarity_search(question, embeds):
+    # embed question
+    # similarity search for 
+    return docm
+
+def talk_to_proposals():
+
+    return response
+
+openai.api_key = st.text_input("OpenAI API Key", type="password")
+question = st.text_input("Talk to Lido proposals")
+
+query_proposals()
