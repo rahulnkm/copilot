8256113a88019aeac41e42e6f39f5cb4eac28412
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2023-10-10 23:26:28 -0400

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+st.header("Lido Copilot")
+
+"""
+Everything you need to know about Lido Finance.
+We've passed all prior proposals with all relevant voting information.
+Use it to get proposal summaries.
+Enter your personalized information to get suggestions.
+"""
