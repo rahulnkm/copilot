76a9f21ed3f726648043caeb266e6e69f3a7df04
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2023-10-12 02:24:47 -0400

    go

--- main.py
+++ main.py
@@ -91,7 +91,8 @@ def supabase_search(question): # CALLS EMBED FROM SUPABASE
     for x in embeds:
         e = x["embed"]
         a = np.array(e)
-        return a.shape, a.dtype
+        agh = [0, 2, 3]
+        return agh.shape, agh.dtype
         final.append(e)
     for f in final:
         a = np.array(q)
