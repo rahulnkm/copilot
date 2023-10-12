7c2379f6eec073d473f98e03bee55f9b37b2bffa
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2023-10-12 01:21:00 -0400

    try

--- main.py
+++ main.py
@@ -97,6 +97,8 @@ def similarity_search(question, embeds): # FAILS: CANT RETURN TEXT ARRAY - PASS 
 
 def supabase_search(question): # CALLS EMBED FROM SUPABASE
     q = embed_docm(question)
+    return q
+
     emb = supabase.table('lido').select("embed").execute()
     embeds = emb.data
     return embeds
