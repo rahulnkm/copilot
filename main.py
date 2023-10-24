5585198915a79b1a93de1c6a13f988b9cd649784
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2023-10-24 09:13:28 -0400

    go

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def supabase_search(question): # CALLS EMBED FROM SUPABASE
     final = []
     for x in embeds:
         e = x["embed"]
-        return e
+        return e.length
         a = np.array(e)
         return a
         final.append(e)
